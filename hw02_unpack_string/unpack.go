10835a4f0408d7990374c2fba69b4e730477c2ca
postemp <apostemp@mail.ru>
2022-06-10 16:46:28 +0300

    HW02 main part is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,7 +9,7 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(inputStr string) (string, error) {
+func Unpack(inputStr string) (string, error) { //nolint:gocognit
 	var builtStr strings.Builder
 	chars := []rune(inputStr)
 	for i := 0; i < len(chars); i++ {
@@ -25,8 +25,22 @@ func Unpack(inputStr string) (string, error) {
 		if unicode.IsDigit(chars[i]) && unicode.IsDigit(chars[i+1]) {
 			return "", errors.New("некорректная строка")
 		}
-		// if chars[i] == 92 { return "\\", errors.New("символ 92") }
-		if i+1 < len(chars) && unicode.IsDigit(chars[i+1]) { // checking if a next symbol is a digit
+		// checking if a symbol is '\' - 92
+		if chars[i] == 92 && (unicode.IsDigit(chars[i+1]) || chars[i+1] == 92) {
+			if i+2 < len(chars) && unicode.IsDigit(chars[i+2]) {
+				repeatTimes, err := strconv.Atoi(string(chars[i+2]))
+				if err != nil {
+					return "", errors.New("ошибка при конвертации символа в цифру")
+				}
+				builtStr.WriteString(strings.Repeat(string(chars[i+1]), repeatTimes))
+				i++
+				continue
+			}
+			builtStr.WriteString(string(chars[i+1]))
+			i++
+			continue
+		}
+		if i+1 < len(chars) && unicode.IsDigit(chars[i+1]) { // checking if the next symbol is a digit
 			repeatTimes, err := strconv.Atoi(string(chars[i+1]))
 			if err != nil {
 				return "", errors.New("ошибка при конвертации символа в цифру")
@@ -39,6 +53,5 @@ func Unpack(inputStr string) (string, error) {
 		}
 		builtStr.WriteString(string(chars[i]))
 	}
-
 	return builtStr.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -20,11 +20,11 @@ func TestUnpack(t *testing.T) {
 		{input: "qw\n2e", expected: "qw\n\ne"},
 		{input: "\n2", expected: "\n\n"},
 		// uncomment if task with asterisk completed
-		// {input: `e\3`, expected: `e3`},
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `e\33`, expected: `e333`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
